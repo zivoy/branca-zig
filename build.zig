399581bc4d7b6bc614041f244ef4d3a5b3929c53
zivoy <ziv132@gmail.com>
2024-04-22 23:30:33 -0400

    modules

--- build.zig
+++ build.zig
@@ -4,15 +4,23 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
+    const baseX = b.dependency("baseX", .{
+        .target = target,
+        .optimize = optimize,
+    });
+
+    _ = b.addModule("branca", .{
+        .target = target,
+        .optimize = optimize,
+        .root_source_file = b.path("src/branca.zig"),
+    });
     const lib = b.addStaticLibrary(.{
-        .name = "branca-zig",
+        .name = "branca",
         .root_source_file = b.path("src/branca.zig"),
         .target = target,
         .optimize = optimize,
     });
-    lib.root_module.addAnonymousImport("base-x", .{
-        .root_source_file = b.path("libs/baseX/baseX.zig"),
-    });
+    lib.root_module.addImport("base-x", baseX.module("baseX"));
     b.installArtifact(lib);
 
     const lib_unit_tests = b.addTest(.{
@@ -20,9 +28,7 @@ pub fn build(b: *std.Build) void {
         .target = target,
         .optimize = optimize,
     });
-    lib_unit_tests.root_module.addAnonymousImport("base-x", .{
-        .root_source_file = b.path("libs/baseX/baseX.zig"),
-    });
+    lib_unit_tests.root_module.addImport("base-x", baseX.module("baseX"));
     const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
 
     const test_step = b.step("test", "Run unit tests");
